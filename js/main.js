7386944fd1742fa15822d139efefad956584fd52
TFuray <pk.master881@gmail.com>
2022-09-30 00:49:14 -0700

    removed choose catagory options

--- js/main.js
+++ js/main.js
@@ -1,28 +1,30 @@
 
-document.querySelector('#show-cats').addEventListener('click', showCats)
+// document.querySelector('#show-cats').addEventListener('click', showCats)
 document.querySelector('#random').addEventListener('click', random)
 // display list of categories to choose in dom
-function showCats() {
-    const cats = fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`)
-    cats
-        .then(res => res.json())
-        .then(data => {
-            let catList = data.categories
-            console.log(catList[0].strCategory);
-            let list = document.getElementById('catList')
-            for (let i = 0; i < catList.length; i++) {
-                let li = document.createElement('li')
-                li.classList.add('cat-list')
-                li.setAttribute('id', `${catList[i].strCategory})`)
-                li.innerText = catList[i].strCategory
-                li.addEventListener('click', )
-                list.appendChild(li)
-            }
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-        })
-}
+// function showCats() {
+//     const cats = fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`)
+//     cats
+//         .then(res => res.json())
+//         .then(data => {
+//             let catList = data.categories
+//             let list = document.getElementById('catList')
+//             for (let i = 0; i < catList.length; i++) {
+//                 let li = document.createElement('li')
+//                 let id = catList[i].strCategory
+//                 li.classList.add('cat-list')
+//                 li.addEventListener('click', pickCat(`${catList[i].strCategory}`))
+//                 li.setAttribute('id', `${catList[i].strCategory})`)
+//                 li.innerText = catList[i].strCategory
+//                 list.appendChild(li)
+//             }
+            
+            
+//         })
+//         .catch(err => {
+//             console.log(`error ${err}`)
+//         })
+// }
 
 function random() {
     fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
@@ -43,19 +45,29 @@ function random() {
                 }
             }
             document.querySelector('#instructions').innerText = `${meal.strInstructions}`
+            document.querySelector('#random').addEventListener('click', resetRandom)
         })
 }
 
-
-function pickCat(cat) {
-    let url = `www.themealdb.com/api/json/v1/1/filter.php?c=${cat}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-        })
+function resetRandom() {
+    document.querySelector('#display').classList.toggle('hidden')
+    document.querySelector('#mealName').innerText = null
+    document.querySelector('#mealPic').src = null
+    document.querySelector('#instructions').innerText = null
+    document.querySelector('#ing').innerText = null
 }
+
+// function pickCat(cat) {
+//     let url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${cat}`
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => {
+//             resetRandom()
+//             document.querySelector('#display').classList.toggle('hidden')
+
+
+//         })
+//         .catch(err => {
+//             console.log(`error ${err}`)
+//         })
+// }
